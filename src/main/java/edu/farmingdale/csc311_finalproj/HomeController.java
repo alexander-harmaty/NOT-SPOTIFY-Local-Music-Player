7815da061c12e61e320b53de6bffadd10f204565
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-12-05 02:19:02 -0500

    queue

--- src/main/java/edu/farmingdale/csc311_finalproj/HomeController.java
+++ src/main/java/edu/farmingdale/csc311_finalproj/HomeController.java
@@ -5,13 +5,17 @@ import com.mpatric.mp3agic.ID3v2;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
+import com.sun.javafx.font.FontResource;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXListView;
 import io.github.palexdev.materialfx.controls.MFXSlider;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import io.github.palexdev.materialfx.filter.IntegerFilter;
 import io.github.palexdev.materialfx.filter.StringFilter;
+import io.github.palexdev.materialfx.font.FontResources;
+import io.github.palexdev.materialfx.font.MFXFontIcon;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -26,27 +30,36 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
+import javax.swing.Icon;
 
 public class HomeController implements Initializable {
 
@@ -57,7 +70,8 @@ public class HomeController implements Initializable {
     private MFXButton button_library;
 
     @FXML
-    private MFXButton button_next, button_previous, button_togglePlayPause;
+    private MFXButton button_next, button_shuffle, button_queue, button_clearQueue,
+            button_togglePlayPause;
 
     @FXML
     private ImageView imageView_albumArtBar, imageView_albumArtMain;
@@ -77,18 +91,25 @@ public class HomeController implements Initializable {
     @FXML
     private MFXTableView<Song> tableView_songsList;
     
+    @FXML
+    private MFXListView<String> listView_queue;
+    
     ////////////////////////////////////////////////////////////////////////////
     
-    ObservableList<Song> observableList_librarySongs;
+    ObservableList<Song> observableList_songsList;
+    ObservableList<String> observableList_queueSongs;
     Set<Song> set_librarySongs = new HashSet<>();
+    List<Song> list_queue = new ArrayList<>();
+    List<Song> list_currentPlayList = new ArrayList<>();
     
     ////////////////////////////////////////////////////////////////////////////
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-        //set column comparator rules to for the tableView
+        //set column comparator and click rules for the tableView
         setupTable();
+        //setOnMousePressed();
         
         //set initial tableView data to show user library
         try {
@@ -123,9 +144,10 @@ public class HomeController implements Initializable {
     }
     
     void insertIntoTable(Collection collection) {
-        observableList_librarySongs = FXCollections.observableArrayList(collection);
+        observableList_songsList = FXCollections.observableArrayList(collection);
         tableView_songsList.getItems().clear();
-        tableView_songsList.setItems(observableList_librarySongs);
+        tableView_songsList.setItems(observableList_songsList);
+        list_currentPlayList.addAll(collection);
     }
     
     void updateLibrarySongsSetList() throws IOException, UnsupportedTagException, InvalidDataException {
@@ -145,6 +167,29 @@ public class HomeController implements Initializable {
             }
         } catch (SQLException except) {}
         
+    }
+    
+    @FXML
+    void setOnMousePressed() {
+        
+        tableView_songsList.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //check for primary mouse clicks
+                if (event.getButton().equals(MouseButton.PRIMARY)) {
+                    
+                    //if click is double click...
+                    if (event.getClickCount() == 2) {
+                        
+                        //read selected course CRN
+                        ObservableMap<Integer, Song> map;
+                        map = tableView_songsList.getSelectionModel().getSelection();
+                        Song song = new Song(map.get(0));
+                        System.out.println(song.toString());
+                    }
+                }
+            }    
+        });
     }
     
     ////////////////////////////////////////////////////////////////////////////
@@ -171,7 +216,7 @@ public class HomeController implements Initializable {
                 song.writeToDB();
             }
             
-            if (tableView_songsList.getItems() == observableList_librarySongs) {
+            if (tableView_songsList.getItems() == observableList_songsList) {
                 //read new data into Library set for songs
                 updateLibrarySongsSetList();
                 insertIntoTable(set_librarySongs);
@@ -224,16 +269,13 @@ public class HomeController implements Initializable {
     
     @FXML
     void handleButton_library(ActionEvent event) throws IOException, UnsupportedTagException, UnsupportedTagException, InvalidDataException{
-        updateLibrarySongsSetList();
-        insertIntoTable(set_librarySongs);
+        try {
+            updateLibrarySongsSetList();
+            insertIntoTable(set_librarySongs);
+        } catch (IOException | UnsupportedTagException | InvalidDataException ex) {}
     }
 
     ////////////////////////////////////////////////////////////////////////////
-    
-    @FXML
-    void handleButton_previous(ActionEvent event) {
-
-    }
 
     @FXML
     void handleButton_togglePlayPause(ActionEvent event) {
@@ -243,6 +285,35 @@ public class HomeController implements Initializable {
     @FXML
     void handleButton_next(ActionEvent event) {
 
+    }
+    
+    @FXML
+    void handleButton_queue(ActionEvent event) {
+        
+        list_queue.addAll(list_currentPlayList) ;
+        
+        List<String> queueStrings = new ArrayList<>();
+        List<Song> tempList = new ArrayList<>();
+        tempList.addAll(list_currentPlayList);
+        while (!tempList.isEmpty()) {
+            Song tempSong = new Song(tempList.remove(0));
+            String songTitleAndArtist = tempSong.getSongTitle() + " by " + tempSong.getSongArtist();
+            queueStrings.add(songTitleAndArtist);
+        }
+        
+        observableList_queueSongs = FXCollections.observableArrayList(queueStrings);
+        listView_queue.setItems(observableList_queueSongs);
+    }
+
+    @FXML
+    void handleButton_shuffle(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void handleButton_clearQueue(ActionEvent event) {
+        listView_queue.getItems().clear();
+        list_queue.clear();
     }
 
     ////////////////////////////////////////////////////////////////////////////
